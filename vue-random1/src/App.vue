a4b8df33694d6ff363eabd7f46621ef902f83390
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-09 10:46:17 +0600

    1.1_computed setters(a)

--- vue-random1/src/App.vue
+++ vue-random1/src/App.vue
@@ -1,13 +1,18 @@
 <template lang="">
   <div>
       <TestComp />
+      <br>
+      <Exercise202 />
   </div>
 </template>
 <script>
 import TestComp from './components/TestComp.vue'
+import Exercise202 from './components/Exercise2-02.vue'
   export default{
     components: {
       TestComp,
+      Exercise202,
+
     }
 
   }
---
+++ vue-random1/src/components/Exercise2-02.vue
@@ -0,0 +1,35 @@
+<template lang="">
+    <div class="container">
+        <h1>using computed setters</h1>
+        <input type="number" v-model="incrementOne">
+        <h3>Get input: {{ incrementOne}}</h3>
+
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                count: -1,
+            }
+
+        },
+        computed: {
+            incrementOne: {
+                //getter
+                get(){
+                    return this.count + 1
+                },
+                //setter
+                set(val){
+                    this.count = val -1
+
+                },
+
+            },
+
+        },
+
+    }
+
+</script>
