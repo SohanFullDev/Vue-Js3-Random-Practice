36e23b81a1138a230d3a9e855dc6e565ba149b5c
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-09 15:21:15 +0600

    1.6_using asynchronous methods to retrieve data from an api-testing

--- vue-random1/src/App.vue
+++ vue-random1/src/App.vue
@@ -1,4 +1,4 @@
-<template lang="">
+<template>
   <div>
       <TestComp />
       <br>
@@ -7,6 +7,8 @@
       <Exercise203 />
       <br>
       <Exercise204 />
+      <br>
+      <Exercise205 />
 
   </div>
 </template>
@@ -15,12 +17,14 @@ import TestComp from './components/TestComp.vue'
 import Exercise202 from './components/Exercise2-02.vue'
 import Exercise203 from './components/Exercise2-03.vue'
 import Exercise204 from './components/Exercise2-04.vue'
+import Exercise205 from './components/Exercise2-05.vue'
   export default{
     components: {
       TestComp,
       Exercise202,
       Exercise203,
-      Exercise204
+      Exercise204,
+      Exercise205
 
     }
 
