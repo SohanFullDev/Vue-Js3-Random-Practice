743c8fb23a94072c40be020cdbeca414074c9d77
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-09 13:08:02 +0600

    1.4_watching nested properties of a data object(a)_e2.04

--- vue-random1/src/App.vue
+++ vue-random1/src/App.vue
@@ -5,17 +5,22 @@
       <Exercise202 />
       <br>
       <Exercise203 />
+      <br>
+      <Exercise204 />
+
   </div>
 </template>
 <script>
 import TestComp from './components/TestComp.vue'
 import Exercise202 from './components/Exercise2-02.vue'
 import Exercise203 from './components/Exercise2-03.vue'
+import Exercise204 from './components/Exercise2-04.vue'
   export default{
     components: {
       TestComp,
       Exercise202,
-      Exercise203
+      Exercise203,
+      Exercise204
 
     }
 
---
+++ vue-random1/src/components/Exercise2-04.vue
@@ -0,0 +1,57 @@
+<template lang="">
+    <div class="container">
+        <h1>Watching nested properties of a data object</h1>
+        <h2>Deep Watcher</h2>
+        <div>
+            <h4>{{ product.label}}</h4>
+            <h5>${{ product.price}} (${{ discount }} off)</h5>
+            <button @click="updatePrice">Reduce Price!</button>
+        </div>
+        
+
+    </div>
+    
+</template>
+<script>
+export default {
+    data(){
+        return {
+            discount: 0,
+            product: {
+                price:25,
+                label: 'Blue juice',
+            },
+
+        }
+
+    },
+    methods:{
+        updatePrice(){
+            if(this.product.price <1)  return
+            this.product.price--
+        }
+    },
+
+}
+
+</script>
+<style scoped>
+    .container{
+        margin: 0 auto;
+        padding: 30px;
+        max-width: 600px;
+        font-family: 'Avenir', Helvetica, sans-serif;
+        margin: 0;
+    }
+    button{
+        display: inline-block;
+        background-color: rgb(235, 50, 50);
+        border-radius: 10px;
+        font-size: 14px;
+        color: white;
+        padding: 10px 20px;
+        text-decoration: none;
+
+    }
+
+</style>
